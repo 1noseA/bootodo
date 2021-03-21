739dbe3c94caf906f1d95815ac6de76aa13597c6
1noseA <1noseA2L@gmail.com>
2021-03-21 20:32:20 +0900

    HTMLとコントローラ追加

---
+++ src/main/java/controller/TodoController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import entity.Todo;
+import mapper.TodoMapper;
+
+@Controller
+public class TodoController {
+
+	@Autowired
+	TodoMapper todoMapper;
+
+	@RequestMapping(value="/")
+	public String index(Model model) {
+		List<Todo> list = todoMapper.selectAll();
+		model.addAttribute("todos", list);
+		return "index";
+	}
+}
