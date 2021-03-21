b347bb5f7b7697c8ad9b360c43422611953c9d5a
1noseA <1noseA2L@gmail.com>
2021-03-21 19:39:36 +0900

    Mapperの作成

---
+++ src/main/java/mapper/TodoMapper.java
@@ -0,0 +1,14 @@
+package mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import entity.Todo;
+
+@Mapper
+public interface TodoMapper {
+
+	public List<Todo> selectAll();
+
+}
